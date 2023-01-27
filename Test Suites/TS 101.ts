e0994e6d7ead760747079b5da323decbec3f196e
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-01-27 11:44:43 +0700

    first commit

---
+++ Test Suites/TS 101.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS 101</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9ea960d8-1df9-4e2a-ace5-b36fcc82c7bd</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c09b13a-4efc-48c1-817a-925d4bdd85d0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 103 Search Exception</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
